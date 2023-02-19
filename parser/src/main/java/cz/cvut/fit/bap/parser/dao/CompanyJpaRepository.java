3d5e6a2186b88d47c63b22cea784c8e37c5edaa5
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-02-19 11:36:50 +0100

    Adding findByName method

--- parser/src/main/java/cz/cvut/fit/bap/parser/dao/CompanyJpaRepository.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/dao/CompanyJpaRepository.java
@@ -4,6 +4,9 @@ import cz.cvut.fit.bap.parser.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyJpaRepository extends JpaRepository<Company, Long>{
+    Optional<Company> findCompanyByName(String name);
 }
--- parser/src/main/java/cz/cvut/fit/bap/parser/dao/ContractorAuthorityJpaRepository.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/dao/ContractorAuthorityJpaRepository.java
@@ -4,6 +4,9 @@ import cz.cvut.fit.bap.parser.domain.ContractorAuthority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContractorAuthorityJpaRepository extends JpaRepository<ContractorAuthority, Long>{
+    Optional<ContractorAuthority> findContractorAuthorityByName(String name);
 }
