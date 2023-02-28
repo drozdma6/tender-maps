fbf026fd9fc22079afb4b18f34e246a6729c39d9
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-02-28 13:01:03 +0100

    Running application without running web server

--- parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
@@ -1,11 +1,17 @@
 package cz.cvut.fit.bap.parser;
 
+import cz.cvut.fit.bap.parser.business.scrapper.MainScrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ParserApplication{
-    public static void main(String[] args){
-        SpringApplication.run(ParserApplication.class, args);
+    public static void main(String[] args) throws IOException{
+        ApplicationContext ctx = SpringApplication.run(ParserApplication.class, args);
+        MainScrapper mainScrapper = ctx.getBean(MainScrapper.class);
+        mainScrapper.scrape();
     }
 }
