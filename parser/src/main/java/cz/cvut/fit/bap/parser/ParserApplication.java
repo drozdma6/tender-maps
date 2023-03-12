3073109c7725141266ece23ae419e1a92c6bd3f2
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-03-13 00:14:23 +0100

    Fixing scheduling bug

--- parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
@@ -1,19 +1,13 @@
 package cz.cvut.fit.bap.parser;
 
-import cz.cvut.fit.bap.parser.scrapper.MainScrapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
 public class ParserApplication{
-    public static void main(String[] args) throws IOException{
-        ApplicationContext ctx = SpringApplication.run(ParserApplication.class, args);
-        MainScrapper mainScrapper = ctx.getBean(MainScrapper.class);
-        mainScrapper.scrape();
+    public static void main(String[] args){
+        SpringApplication.run(ParserApplication.class, args);
     }
 }
