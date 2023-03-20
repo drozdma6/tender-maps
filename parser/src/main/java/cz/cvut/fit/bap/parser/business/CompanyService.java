521a594e171927749885bd41f376e9c9d90366be
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-03-20 14:03:32 +0100

    Adding documentation to services

--- parser/src/main/java/cz/cvut/fit/bap/parser/business/CompanyService.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/business/CompanyService.java
@@ -6,11 +6,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * Class handling communication with company repository
+ */
 @Service
 public class CompanyService extends AbstractCreateService<Company, Long>{
     public CompanyService(CompanyJpaRepository repository){
         super(repository);
     }
+
+    /**
+     * Reads company from database by name
+     *
+     * @param name of wanted company
+     * @return optional of company
+     */
     public Optional<Company> readByName(String name){
         return ((CompanyJpaRepository) repository).findCompanyByName(name);
     }
--- parser/src/main/java/cz/cvut/fit/bap/parser/business/OfferService.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/business/OfferService.java
@@ -5,9 +5,12 @@ import cz.cvut.fit.bap.parser.domain.Offer;
 import cz.cvut.fit.bap.parser.domain.OfferId;
 import org.springframework.stereotype.Service;
 
+/**
+ * Class handling communication with offer repository
+ */
 @Service
 public class OfferService extends AbstractCreateService<Offer, OfferId>{
-    protected OfferService(OfferJpaRepository repository){
+    public OfferService(OfferJpaRepository repository){
         super(repository);
     }
 }
