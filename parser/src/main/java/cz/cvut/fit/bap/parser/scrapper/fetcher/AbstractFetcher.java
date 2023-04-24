a09c05c36094c8b354598ca0bfa6967c951fb395
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-04-24 23:39:18 +0200

    Removing IOException

--- parser/src/main/java/cz/cvut/fit/bap/parser/scrapper/fetcher/AbstractFetcher.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/scrapper/fetcher/AbstractFetcher.java
@@ -3,17 +3,16 @@ package cz.cvut.fit.bap.parser.scrapper.fetcher;
 
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
 
 public abstract class AbstractFetcher{
-    public abstract Document getContractorDetail(String profile) throws IOException;
+    public abstract Document getContractorDetail(String profile);
 
     public abstract Document getContractorCompleted(String profile, String contractorAuthorityName,
-                                                    int iterationCounter) throws IOException;
+                                                    Integer iterationCounter);
 
-    public abstract Document getProcurementResult(String procurement) throws IOException;
+    public abstract Document getProcurementResult(String procurement);
 
-    public abstract Document getCompanyDetail(String uri) throws IOException;
+    public abstract Document getCompanyDetail(String uri);
 
-    public abstract Document getProcurementDetail(String procurement) throws IOException;
+    public abstract Document getProcurementDetail(String procurement);
 }
