7b72d0f60d5631024fefbf0c54eb000e49c9bb8b
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-04-29 13:37:13 +0200

    Moving geocoding to controller

--- parser/src/main/java/cz/cvut/fit/bap/parser/business/AddressService.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/business/AddressService.java
@@ -1,14 +1,10 @@
 package cz.cvut.fit.bap.parser.business;
 
-import cz.cvut.fit.bap.parser.business.Geocoder.GoogleGeocodingApi;
-import cz.cvut.fit.bap.parser.business.Geocoder.ProfinitGeocodingApi;
+import cz.cvut.fit.bap.parser.controller.dto.AddressDto;
 import cz.cvut.fit.bap.parser.dao.AddressJpaRepository;
 import cz.cvut.fit.bap.parser.domain.Address;
-import cz.cvut.fit.bap.parser.scrapper.dto.AddressDto;
-import cz.cvut.fit.bap.parser.scrapper.dto.converter.AddressDtoToAddress;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -16,51 +12,8 @@ import java.util.Optional;
  */
 @Service
 public class AddressService extends AbstractCreateService<Address, Long>{
-    private final GoogleGeocodingApi googleGeocoder;
-    private final ProfinitGeocodingApi profinitGeocoder;
-
-    private final AddressDtoToAddress addressDtoToAddress;
-
-    public AddressService(AddressJpaRepository addressJpaRepository,
-                          GoogleGeocodingApi googleGeocoder, ProfinitGeocodingApi profinitGeocoder,
-                          AddressDtoToAddress addressDtoToAddress){
+    public AddressService(AddressJpaRepository addressJpaRepository){
         super(addressJpaRepository);
-        this.googleGeocoder = googleGeocoder;
-        this.profinitGeocoder = profinitGeocoder;
-        this.addressDtoToAddress = addressDtoToAddress;
-    }
-
-    /*
-        Forbids creating entity from Address
-     */
-    @Override
-    public Address create(Address entity){
-        throw new UnsupportedOperationException("Unsupported operation create in address");
-    }
-
-    /**
-     * Creates new address entity from addressDto by setting country code, latitude and longitude
-     *
-     * @param addressDto which is supposed to be stored
-     * @return address from database
-     */
-    public Address create(AddressDto addressDto){
-        Optional<Address> addressOptional = readAddress(addressDto);
-        if (addressOptional.isPresent()){
-            return addressOptional.get();
-        }
-        if (dtoIsIncomplete(addressDto)){
-            return super.create(addressDtoToAddress.apply(addressDto));
-        }
-        Address address;
-        String country = addressDto.getCountry().toLowerCase();
-        if (Objects.equals(country, "cz")){
-            //profinit geocoder for czech places
-            address = profinitGeocoder.geocode(addressDto);
-        } else{
-            address = googleGeocoder.geocode(addressDto);
-        }
-        return super.create(address);
     }
 
     /**
@@ -71,12 +24,6 @@ public class AddressService extends AbstractCreateService<Address, Long>{
      */
     public Optional<Address> readAddress(AddressDto address){
         return ((AddressJpaRepository) repository).readAddress(address);
-    }
-
-    private boolean dtoIsIncomplete(AddressDto addressDto){
-        return addressDto.getCountry() == null || addressDto.getBuildingNumber() == null ||
-               addressDto.getCity() == null || addressDto.getStreet() == null ||
-               addressDto.getPostalCode() == null;
     }
 }
 
--- parser/src/main/java/cz/cvut/fit/bap/parser/controller/AddressController.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/controller/AddressController.java
@@ -1,26 +1,61 @@
 package cz.cvut.fit.bap.parser.controller;
 
 import cz.cvut.fit.bap.parser.business.AddressService;
+import cz.cvut.fit.bap.parser.controller.Geocoder.GoogleGeocodingApi;
+import cz.cvut.fit.bap.parser.controller.Geocoder.ProfinitGeocodingApi;
+import cz.cvut.fit.bap.parser.controller.dto.AddressDto;
+import cz.cvut.fit.bap.parser.controller.dto.converter.AddressDtoToAddress;
 import cz.cvut.fit.bap.parser.domain.Address;
-import cz.cvut.fit.bap.parser.scrapper.dto.AddressDto;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
 
 /*
     Controller for communication with address service
  */
 @Component
 public class AddressController extends AbstractController<AddressService>{
-    public AddressController(AddressService addressService){
+    private final AddressDtoToAddress addressDtoToAddress;
+    private final ProfinitGeocodingApi profinitGeocodingApi;
+    private final GoogleGeocodingApi googleGeocodingApi;
+
+    public AddressController(AddressService addressService, AddressDtoToAddress addressDtoToAddress, ProfinitGeocodingApi profinitGeocodingApi, GoogleGeocodingApi googleGeocodingApi){
         super(addressService);
+        this.addressDtoToAddress = addressDtoToAddress;
+        this.profinitGeocodingApi = profinitGeocodingApi;
+        this.googleGeocodingApi = googleGeocodingApi;
     }
 
     /**
-     * Saves address from addressDto
+     * Creates new address entity from addressDto by setting country code, latitude and longitude
      *
-     * @param addressDto which is supposed to get saved
-     * @return saved address
+     * @param addressDto which is supposed to be stored
+     * @return address from database
      */
     public Address saveAddress(AddressDto addressDto){
-        return service.create(addressDto);
+        Optional<Address> addressOptional = service.readAddress(addressDto);
+        if(addressOptional.isPresent()){
+            return addressOptional.get();
+        }
+        if(dtoIsIncomplete(addressDto)){
+            return service.create(addressDtoToAddress.apply(addressDto));
+        }
+        Address address;
+        String country = addressDto.getCountry().toLowerCase();
+        if(Objects.equals(country, "cz")){
+            //profinit geocoder for czech places
+            address = profinitGeocodingApi.geocode(addressDto);
+        }else{
+            //google geocoder for foreign countries
+            address = googleGeocodingApi.geocode(addressDto);
+        }
+        return service.create(address);
+    }
+
+    private boolean dtoIsIncomplete(AddressDto addressDto){
+        return addressDto.getCountry() == null || addressDto.getBuildingNumber() == null ||
+                addressDto.getCity() == null || addressDto.getStreet() == null ||
+                addressDto.getPostalCode() == null;
     }
 }
