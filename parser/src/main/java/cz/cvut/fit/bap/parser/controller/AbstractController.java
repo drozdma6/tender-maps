b1a39ab24f2bfaca12d1540160b5f580cf423090
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-05-12 20:58:08 +0200

    Moving removeUrlParameters to scrapper

--- parser/src/main/java/cz/cvut/fit/bap/parser/controller/AbstractController.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/controller/AbstractController.java
@@ -1,8 +1,5 @@
 package cz.cvut.fit.bap.parser.controller;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /*
     Abstract controller with some service
  */
@@ -11,30 +8,5 @@ public abstract class AbstractController<S>{
 
     public AbstractController(S service){
         this.service = service;
-    }
-
-    /**
-     * Removes URL parameters starting with "/p:" and continuing until the next "/" character,
-     * if the /p: parameter is at the end it stays.
-     *
-     * @param url the URL string to modify
-     * @return the modified URL string with "/p:" parameters removed or
-     * returns the original URL string if no parameters found
-     */
-    protected String removeUrlParameters(String url){
-        Pattern pattern = Pattern.compile("/p:.*?/");
-        Matcher matcher = pattern.matcher(url);
-        StringBuilder modifiedUrl = new StringBuilder(url);
-
-        while(matcher.find()){
-            int startIndex = matcher.start();
-            int endIndex = matcher.end();
-
-            if(endIndex < modifiedUrl.length()){
-                modifiedUrl.delete(startIndex, endIndex - 1);
-                matcher.reset(modifiedUrl);
-            }
-        }
-        return modifiedUrl.toString();
     }
 }
--- parser/src/main/java/cz/cvut/fit/bap/parser/controller/scrapper/AbstractScrapper.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/controller/scrapper/AbstractScrapper.java
@@ -2,6 +2,9 @@ package cz.cvut.fit.bap.parser.controller.scrapper;
 
 import org.jsoup.nodes.Document;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public abstract class AbstractScrapper{
     protected Document document;
@@ -15,5 +18,30 @@ public abstract class AbstractScrapper{
             return null;
         }
         return str;
+    }
+
+    /**
+     * Removes URL parameters starting with "/p:" and continuing until the next "/" character,
+     * if the /p: parameter is at the end it stays.
+     *
+     * @param url the URL string to modify
+     * @return the modified URL string with "/p:" parameters removed or
+     * returns the original URL string if no such parameters found
+     */
+    protected String removeUrlParameters(String url){
+        Pattern pattern = Pattern.compile("/p:.*?/");
+        Matcher matcher = pattern.matcher(url);
+        StringBuilder modifiedUrl = new StringBuilder(url);
+
+        while(matcher.find()){
+            int startIndex = matcher.start();
+            int endIndex = matcher.end();
+
+            if(endIndex < modifiedUrl.length()){
+                modifiedUrl.delete(startIndex, endIndex - 1);
+                matcher.reset(modifiedUrl);
+            }
+        }
+        return modifiedUrl.toString();
     }
 }
