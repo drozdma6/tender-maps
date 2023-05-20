1b7d30f4f0e0c1acb7898e0f33085ddffa2f7df9
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-05-20 21:31:13 +0200

    Removing unnecessary id check

--- parser/src/main/java/cz/cvut/fit/bap/parser/business/AbstractCreateService.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/business/AbstractCreateService.java
@@ -1,7 +1,6 @@
 package cz.cvut.fit.bap.parser.business;
 
 import cz.cvut.fit.bap.parser.domain.DomainEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -23,11 +22,7 @@ public abstract class AbstractCreateService<E extends DomainEntity<K>, K>{
      * @param entity which is supposed to be stored
      * @return stored entity
      */
-    @Transactional
     public E create(E entity){
-        K id = entity.getId();
-        if (id != null && repository.existsById(id))
-            throw new RuntimeException();
         return repository.save(entity);
     }
 }
