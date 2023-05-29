dbaf8ce062d9cb3b4874d2b13cb017e5c3a1c63b
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-05-29 12:15:00 +0200

    Adding scrapper factories test

---
+++ parser/src/test/java/cz/cvut/fit/bap/parser/controller/scrapper/factories/FactoryScrapperTest.java
@@ -0,0 +1,37 @@
+package cz.cvut.fit.bap.parser.controller.scrapper.factories;
+
+import cz.cvut.fit.bap.parser.controller.scrapper.AbstractScrapper;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+/*
+    Class for testing all factory scrappers
+ */
+class FactoryScrapperTest{
+    private static Stream<AbstractScrapperFactory<?>> factoryProvider(){
+        return Stream.of(
+                new CompanyDetailFactory(),
+                new ContractorCompletedFactory(),
+                new ProcurementDetailFactory(),
+                new ProcurementResultFactory(),
+                new ContractorDetailFactory(),
+                new ContractorListFactory());
+        // Add other factory instances here
+    }
+
+    @ParameterizedTest
+    @MethodSource("factoryProvider")
+    void testFactoryCreation(AbstractScrapperFactory<?> factory){
+        Document document = new Document("testDocument");
+
+        Object scrapper = factory.create(document);
+
+        Assertions.assertNotNull(scrapper);
+        Assertions.assertTrue(scrapper instanceof AbstractScrapper);
+    }
+}
+
