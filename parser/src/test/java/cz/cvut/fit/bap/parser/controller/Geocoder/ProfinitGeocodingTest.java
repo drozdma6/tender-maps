cb6073e90832c5d95f05b661aa125fc528f8cc25
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-05-29 12:15:21 +0200

    Adding geocoding tests

--- parser/src/test/java/cz/cvut/fit/bap/parser/controller/Geocoder/ProfinitGeocodingTest.java
+++ parser/src/test/java/cz/cvut/fit/bap/parser/controller/Geocoder/ProfinitGeocodingTest.java
@@ -1,37 +1,39 @@
-package cz.cvut.fit.bap.parser.business.Integration.Geocoder;
+package cz.cvut.fit.bap.parser.controller.Geocoder;
 
-import cz.cvut.fit.bap.parser.business.Geocoder.GoogleGeocodingApi;
+import cz.cvut.fit.bap.parser.controller.dto.AddressDto;
 import cz.cvut.fit.bap.parser.domain.Address;
-import cz.cvut.fit.bap.parser.scrapper.dto.AddressDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+/*
+    Integration test testing profinit api geocoding.
+ */
 @SpringBootTest
-class GeocodingApiClientTest{
+class ProfinitGeocodingTest{
+
     @Autowired
-    private GoogleGeocodingApi googleGeocodingApi;
+    private ProfinitGeocoding profinitGeocoderApi;
 
     @Test
-    public void geocode(){
-        AddressDto addressDto = new AddressDto("Česká republika", "Praha", "17000", "Čechova",
-                                               "10");
-        Address expectedAddress = new Address("CZ", "Praha", "17000", "Čechova", "10", 50.1006926,
-                                              14.4215078);
-        Address actualAddress = googleGeocodingApi.geocode(addressDto);
+    public void test(){
+        //Čechova 225/10, 17000, Praha 7
+        AddressDto addressDto = new AddressDto("CZ", "Praha", "17000", "Čechova", "10");
+        Address expectedAddress = new Address("CZ", "Praha", "17000", "Čechova", "10",
+                50.10070427864861, 14.42151999409848);
+        Address actualAddress = profinitGeocoderApi.geocode(addressDto);
 
         Assertions.assertEquals(expectedAddress.getCountryCode(), actualAddress.getCountryCode());
         Assertions.assertEquals(expectedAddress.getCity(), actualAddress.getCity());
         Assertions.assertEquals(expectedAddress.getPostalCode(), actualAddress.getPostalCode());
         Assertions.assertEquals(expectedAddress.getStreet(), actualAddress.getStreet());
         Assertions.assertEquals(expectedAddress.getBuildingNumber(),
-                                actualAddress.getBuildingNumber());
+                actualAddress.getBuildingNumber());
         double epsilon = 0.00001;
         Assertions.assertEquals(expectedAddress.getLongitude(), actualAddress.getLongitude(),
-                                epsilon);
+                epsilon);
         Assertions.assertEquals(expectedAddress.getLatitude(), actualAddress.getLatitude(),
-                                epsilon);
+                epsilon);
     }
 }
