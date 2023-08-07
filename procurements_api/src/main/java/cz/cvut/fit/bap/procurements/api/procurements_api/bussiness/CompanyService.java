edd3b3fdbd2f4f8e572672e762bde52a51cf929c
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-08-07 20:24:18 +0200

    Adding companies endpoints

--- procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/bussiness/CompanyService.java
+++ procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/bussiness/CompanyService.java
@@ -1,12 +1,35 @@
 package cz.cvut.fit.bap.procurements.api.procurements_api.bussiness;
 
+import cz.cvut.fit.bap.procurements.api.procurements_api.dao.CompanyRepository;
 import cz.cvut.fit.bap.procurements.api.procurements_api.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import java.util.Collection;
 
+/*
+    Class handling business logic with companies
+ */
 @Service
-public class CompanyService extends AbstractService<Company, Long>{
-    protected CompanyService(JpaRepository<Company,Long> repository){
+public class CompanyService extends AbstractService<Company, Long> {
+    protected CompanyService(JpaRepository<Company, Long> repository) {
         super(repository);
+    }
+
+    /**
+     * Gets all companies which are suppliers of at least one procurement.
+     *
+     * @return all suppliers.
+     */
+    public Collection<Company> getSuppliers() {
+        return ((CompanyRepository) repository).findSuppliers();
+    }
+
+    /**
+     * Gets all companies which have not yet won any procurements.
+     *
+     * @return companies which are not suppliers
+     */
+    public Collection<Company> getCompaniesWithoutWonProcurements() {
+        return ((CompanyRepository) repository).findNonSuppliers();
     }
 }
--- procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/controller/CompanyController.java
+++ procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/controller/CompanyController.java
@@ -1,17 +1,46 @@
 package cz.cvut.fit.bap.procurements.api.procurements_api.controller;
 
 import cz.cvut.fit.bap.procurements.api.procurements_api.bussiness.AbstractService;
+import cz.cvut.fit.bap.procurements.api.procurements_api.bussiness.CompanyService;
 import cz.cvut.fit.bap.procurements.api.procurements_api.controller.dto.CompanyDto;
 import cz.cvut.fit.bap.procurements.api.procurements_api.domain.Company;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.function.Function;
 
+/*
+    Rest controller for companies
+ */
 @RestController
 @RequestMapping("/companies")
-public class CompanyController extends AbstractController<Company, Long,CompanyDto>{
-    protected CompanyController(AbstractService<Company,Long> service, Function<Company,CompanyDto> toDtoConverter){
+public class CompanyController extends AbstractController<Company, Long, CompanyDto> {
+    protected CompanyController(AbstractService<Company, Long> service, Function<Company, CompanyDto> toDtoConverter) {
         super(service, toDtoConverter);
+    }
+
+    /**
+     * Gets all companies which are suppliers for at least one procurement.
+     *
+     * @return suppliers
+     */
+    @CrossOrigin
+    @GetMapping("/suppliers")
+    public List<CompanyDto> getSuppliers() {
+        return ((CompanyService) service).getSuppliers().stream().map(toDtoConverter).toList();
+    }
+
+    /**
+     * Gets all companies which have not yet won any procurements.
+     *
+     * @return companies which are not suppliers
+     */
+    @CrossOrigin
+    @GetMapping("/non-suppliers")
+    public List<CompanyDto> getCompaniesWithoutWonProcurements() {
+        return ((CompanyService) service).getCompaniesWithoutWonProcurements().stream().map(toDtoConverter).toList();
     }
 }
