9276d08e048a8e58409ba81afc1198d57c6c68ad
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-09-09 22:30:14 +0200

    Adding readAll authorities

--- procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/controller/ContractorAuthorityController.java
+++ procurements_api/src/main/java/cz/cvut/fit/bap/procurements/api/procurements_api/controller/ContractorAuthorityController.java
@@ -3,15 +3,29 @@ package cz.cvut.fit.bap.procurements.api.procurements_api.controller;
 import cz.cvut.fit.bap.procurements.api.procurements_api.bussiness.AbstractService;
 import cz.cvut.fit.bap.procurements.api.procurements_api.controller.dto.ContractorAuthorityDto;
 import cz.cvut.fit.bap.procurements.api.procurements_api.domain.ContractorAuthority;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.function.Function;
 
 @RestController
 @RequestMapping("/api/authorities")
-public class ContractorAuthorityController extends AbstractController<ContractorAuthority, Long,ContractorAuthorityDto>{
-    protected ContractorAuthorityController(AbstractService<ContractorAuthority,Long> service, Function<ContractorAuthority,ContractorAuthorityDto> toDtoConverter){
+public class ContractorAuthorityController extends AbstractController<ContractorAuthority, Long, ContractorAuthorityDto> {
+    protected ContractorAuthorityController(AbstractService<ContractorAuthority, Long> service, Function<ContractorAuthority, ContractorAuthorityDto> toDtoConverter) {
         super(service, toDtoConverter);
+    }
+
+    /**
+     * Gets all contractor authorities.
+     *
+     * @return dtos of all contractor authorities.
+     */
+    @CrossOrigin
+    @GetMapping
+    public Collection<ContractorAuthorityDto> readAll() {
+        return service.readAll().stream().map(toDtoConverter).toList();
     }
 }
