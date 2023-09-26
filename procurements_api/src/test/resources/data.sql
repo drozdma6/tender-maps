2626abbc2fa87d2ded0a7779b58b11edfcfe284b
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-09-26 19:47:32 +0200

    Replacing contractor_authority by contracting_authority in test data

--- procurements_api/src/test/resources/data.sql
+++ procurements_api/src/test/resources/data.sql
@@ -5,7 +5,7 @@ VALUES
     (3, '789', 'Test City 3', 'Test Street 3', 'CZ', '54321', 51.5074, -0.1278),
     (4, '12', null, null, 'CZ', '54321', null, null);
 
-INSERT INTO contractor_authority (id, contractor_authority_name, address_id, url)
+INSERT INTO contracting_authority (id, contracting_authority_name, address_id, url)
 VALUES
     (1, 'Authority 1', 1, 'http://authority1.example.com'),
     (2, 'Authority 2', 2, 'http://authority2.example.com');
@@ -18,7 +18,7 @@ VALUES
     (4,'Company D', 1, 'ORG004'),
     (5,'Company E', 4, 'ORG005');
 
-INSERT INTO procurement (id,procurement_name, supplier_id, contractor_authority_id, contract_price, place_of_performance, date_of_publication, system_number)
+INSERT INTO procurement (id,procurement_name, supplier_id, contracting_authority_id, contract_price, place_of_performance, date_of_publication, system_number)
 VALUES
     (1,'Procurement 1', 1, 1, 50000.00, 'Test Place 1', '2023-09-15', 'SYS001'),
     (2,'Procurement 2', 2, 2, 120000.50, 'Test Place 2', '2023-09-10', 'SYS002'),
