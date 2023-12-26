979ca70cf2021049beafa94fc3d0f11860a2f77c
drozdma6 <99978787+drozdma6@users.noreply.github.com>
2023-12-26 23:07:22 +0100

    Add retryable and lang to ProfinitGeocoder

--- parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/ParserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -16,6 +17,7 @@ import java.util.concurrent.Executor;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableRetry
 public class ParserApplication{
     @Value("${core.pool.size}")
     private Integer corePoolSize;
--- parser/src/main/java/cz/cvut/fit/bap/parser/controller/geocoder/ProfinitGeocoder.java
+++ parser/src/main/java/cz/cvut/fit/bap/parser/controller/geocoder/ProfinitGeocoder.java
@@ -6,22 +6,20 @@ import cz.cvut.fit.bap.parser.domain.Address;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Metrics;
 import kotlin.Pair;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
 
 /*
     Geocoding api client used for geocoding czech locations
@@ -30,9 +28,20 @@ import static java.time.temporal.ChronoUnit.SECONDS;
 public class ProfinitGeocoder implements Geocoder{
     private static final String BASE_URL = "https://geolokator.profinit.cz";
     private static final String CZECH_SHORT_COUNTRY_CODE = "CZ";
+    private static final String LANGUAGE_PARAM = "en";
+    private static final long CONNECTION_TIMEOUT_SEC = 10;
+    private static final long REQUEST_TIMEOUT_SEC = 20;
+
+    private final HttpClient httpClient;
 
     @Value("${PROFINIT_API_KEY}")
     private String apiToken;
+
+    public ProfinitGeocoder() {
+        this.httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(CONNECTION_TIMEOUT_SEC))
+                .build();
+    }
 
     /**
      * Geocodes address by calling profinit geocoding api.
@@ -46,7 +55,7 @@ public class ProfinitGeocoder implements Geocoder{
         AddressBuilder addressBuilder = new AddressBuilder(addressData);
         addressBuilder.countryCode(CZECH_SHORT_COUNTRY_CODE); //profinit geocoder is used only for czech places
         try{
-            String response = sendQueryRequest(addressData);
+            JSONObject response = sendQueryRequest(addressData);
             Pair<Double,Double> coordinates = getWgsCoordinates(response);
             addressBuilder.latitude(coordinates.getFirst()); //wgs_x
             addressBuilder.longitude(coordinates.getSecond()); //wgx_y
@@ -59,31 +68,30 @@ public class ProfinitGeocoder implements Geocoder{
     }
 
     /**
-     * Sends request to geocoder
+     * Sends request to geocoder. In case of GeocodingException, function retries the API request.
      *
      * @param addressData which is supposed to be geocoded
      * @return Json response
      */
-    private String sendQueryRequest(AddressData addressData){
-        String addressStr = addressData.street() + ' ' + addressData.buildingNumber() + ", " +
-                addressData.postalCode() + ", " + addressData.city();
-
-        HttpClient httpClient = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
-
-        String queryUrl = BASE_URL + "/query?token=" + URLEncoder.encode(apiToken, StandardCharsets.UTF_8) +
-                "&query=" + URLEncoder.encode(addressStr, StandardCharsets.UTF_8);
-
+    @Retryable(retryFor = GeocodingException.class, maxAttempts = 3, backoff = @Backoff(delay = 1000))
+    private JSONObject sendQueryRequest(AddressData addressData) {
+        String addressStr = String.join(" ", addressData.street(), addressData.buildingNumber(),
+                addressData.postalCode(), addressData.city());
+        URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                .path("query")
+                .queryParam("token", apiToken)
+                .queryParam("query", addressStr)
+                .queryParam("lang", LANGUAGE_PARAM)
+                .build()
+                .toUri();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(queryUrl))
-                .timeout(Duration.of(20, SECONDS))
+                .uri(uri)
+                .timeout(Duration.ofSeconds(REQUEST_TIMEOUT_SEC))
                 .GET()
                 .build();
-
         try{
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return new JSONObject(response.body());
         }catch(IOException e){
             throw new GeocodingException(e);
         }catch(InterruptedException e){
@@ -94,23 +102,16 @@ public class ProfinitGeocoder implements Geocoder{
         }
     }
 
-    private Pair<Double,Double> getWgsCoordinates(String jsonString){
-        JSONObject json = new JSONObject(jsonString);
-        JSONArray results = json.getJSONArray("results");
-
-        if(results.isEmpty()){
-            Metrics.counter("scrapper.profinit.geocoder.failed").increment();
-            return new Pair<>(null, null);
-        }
-        JSONObject firstResult = results.getJSONObject(0);
-        JSONObject ruian = firstResult.getJSONObject("ruian");
-
-        try{
-            return new Pair<>(ruian.getDouble("wgs_x"), ruian.getDouble("wgs_y"));
-        }catch(JSONException e){
-            Metrics.counter("scrapper.profinit.geocoder.failed").increment();
-            //if wgs_x or wgs_y is null
-            return new Pair<>(null, null);
+    private Pair<Double, Double> getWgsCoordinates(JSONObject json) {
+        try {
+            JSONObject ruian = json.getJSONArray("results")
+                    .getJSONObject(0)
+                    .getJSONObject("ruian");
+            double wgsX = ruian.getDouble("wgs_x");
+            double wgsY = ruian.getDouble("wgs_y");
+            return new Pair<>(wgsX, wgsY);
+        } catch (JSONException e) {
+            throw new GeocodingException("Error parsing JSON for coordinates: " + e.getMessage());
         }
     }
 }
